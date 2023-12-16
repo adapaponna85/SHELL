200f2835e01ca3b651492148ac85ef21dbd14784
adapaponna85 <adapaponna85@gmail.com>
2023-12-15 23:14:11 -0500

    added new files

---
+++ date.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+DATE=$(date)
+
+echo "Printing today's date and current system time: ${DATE}"
---
+++ uname_pwd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Enter the user name"
+
+read -s uname
+
+echo "user name entered is: $uname"
+
+echo "Enter the password"
+
+read -s pwd
+
+echo "password entered is: $pwd"
