2e3849c147a631976eff5eccfef32f3566d55d86
adapaponna85 <adapaponna85@gmail.com>
2023-12-17 07:36:45 -0500

    added condition.sh

---
+++ condition.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user=$(id -u)
+
+if ($user -gt 0)
+{ 
+    echo "$user is not authorized to install! please get the root access to proceed."
+}
+else 
+{
+    echo "Proceeding to installation step :"
+}
+fi
+yum install mysql -y
+status=$($?)
+if ($status -gt 0)
+{
+    exit 1
+    echo "installation failed, please verify the logs"
+}
+else
+{
+echo "Installation complete"
+}
+fi
