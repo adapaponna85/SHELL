dea4c23696dfe6772a1efd327b204c7b39bc24e9
adapaponna85 <adapaponna85@gmail.com>
2023-12-17 14:59:14 -0500

    files

--- function.sh
+++ function.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 user=$(id -u)
-echo "$user"
+
+if [ $user -ne 0 ]
+then 
+echo "You are not a root user, login as root user to proceed"
+else
+echo "Proceeding for installation as ROOT user"
+fi
+
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+echo "Installation failed"
+exit 1
+else
+echo "Installation success"
+fi
+
+yum install git -y
+if [ $? -ne 0 ]
+then
+echo "Installation failed"
+exit 1
+else
+echo "Installation success"
+fi
