7891cf203271fbaa00d096c93e3ba64d13dcea64
adapaponna85 <adapaponna85@gmail.com>
2023-12-17 22:59:13 -0500

    files

--- loops.sh
+++ loops.sh
@@ -5,8 +5,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0n"
 
-TIMESTAMP=$(date +%F %H %M %s)
-echo $TIMESTAMP
+TIMESTAMP=$(date +%F-%H-%M-%s)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+then
+echo -e "$R Error: $2 Failed $N"
+else
+echo -e "$G $2 Success $N"
+fi
+}
+
 if [ $user -ne 0 ]
 then
 echo -e "$R You are not authorized, please login as Root user $N"
@@ -14,3 +25,9 @@ exit 1
 else
 echo -e "$G Running as Root user $N"
 fi
+
+yum install git -y &>> LOGFILE
+VALIDATE $? "Installing Git"
+
+yum install mysql -y &>> LOGFILE
+VALIDATE $? "Installing mysql"
